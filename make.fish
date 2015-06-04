b1bf6b3983a0360909e6c65e087f61b3f1df2eca
Peter Bourgon <peter@bourgon.org>
2015-06-04 19:18:37 +0200

    Consistent make.fish

--- make.fish
+++ make.fish
@@ -20,21 +20,21 @@ switch $argv
 		docker ps -a | grep $prefix | awk '{print $1}' | xargs docker stop
 		docker ps -a | grep $prefix | awk '{print $1}' | xargs docker rm
 		docker images | grep $prefix | awk '{print $3}' | xargs docker rmi
-	
+
 	case build
 		docker build -t $prefix-db elasticsearch
 		cd app ; env GOOS=linux GOARCH=amd64 go build ; cd ..
 		docker build -t $prefix-app app
 		docker build -t $prefix-lb nginx
-	
+
 	case run
-		weave run --with-dns --hostname=db.weave.local peterbourgon/tns-db
-		weave run --with-dns --hostname=db.weave.local peterbourgon/tns-db
-		weave run --with-dns --hostname=app.weave.local peterbourgon/tns-app
-		weave run --with-dns --hostname=app.weave.local peterbourgon/tns-app
-		weave run --with-dns --hostname=lb.weave.local peterbourgon/tns-lb
-		weave run --with-dns --hostname=lb.weave.local peterbourgon/tns-lb
-		
+		weave run --with-dns --hostname=db.weave.local $prefix-db
+		weave run --with-dns --hostname=db.weave.local $prefix-db
+		weave run --with-dns --hostname=app.weave.local $prefix-app
+		weave run --with-dns --hostname=app.weave.local $prefix-app
+		weave run --with-dns --hostname=lb.weave.local $prefix-lb
+		weave run --with-dns --hostname=lb.weave.local $prefix-lb
+
 	case '*'
 		usage
 		exit 2
