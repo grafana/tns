1017677489cb2b218d1515e51e847e2ce1160ba5
Joe Elliott <number101010@gmail.com>
2019-11-17 17:29:23 -0500

    Added req.WithContext to Vote
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- app/main.go
+++ app/main.go
@@ -291,6 +291,7 @@ func (a *app) Vote(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	req = req.WithContext(r.Context())
 	resp, err := a.client.Do(req)
 	if err != nil {
 		w.WriteHeader(http.StatusServiceUnavailable)
